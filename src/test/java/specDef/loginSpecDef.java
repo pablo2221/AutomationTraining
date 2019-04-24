342c1e7882dcf7209862f6ae508938e78e7827c3
Pablo Quiros <pablo.2221@hotmail.com>
2019-04-24 16:31:23 -0600

    new

--- src/test/java/specDef/loginSpecDef.java
+++ src/test/java/specDef/loginSpecDef.java
@@ -1,31 +1,67 @@
 package specDef;
 
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.And;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import utilities.driverProvider;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.openqa.selenium.WebDriver;
 
 public class loginSpecDef {
 
+    public WebDriver driver;
+
+    @Before()
+    public void setUp(){
+
+        driver = driverProvider.createDriver("chrome");
+
+    }
+
+    @After()
+    public void tearDown(){
+
+        driver.quit();
+
+    }
+
     @Given("I am on Login Page of {string}")
-    public void iAmOnLoginPageOf(String arg0) {
+    public void iAmOnLoginPageOf(String url) {
+
+        driver.get(url);
+
     }
 
     @When("I enter a valid username {string}")
-    public void iEnterAValidUsername(String arg0) {
+    public void iEnterAValidUsername(String username) {
+
+        driver.findElement(By.id("email")).sendKeys(username);
+
+    }
+
+    @And("I enter a invalid password {string}")
+    public void iEnterAInvalidPassword(String password) {
+
+        driver.findElement(By.id("passwd")).sendKeys(password);
+
     }
 
     @And("I click the sign in button")
     public void iClickTheSignInButton(){
-    }
 
-    @And("I enter a invalid password {string}")
-    public void iEnterAInvalidPassword(String arg0) {
+        driver.findElement(By.className("icon-lock")).click();
 
     }
 
     @Then("I see the error message")
     public void iSeeTheErrorMessage() {
+
+        Assert.assertEquals(driver.findElement(By.className("alert-danger")).isDisplayed(), true);
+
     }
 
 }
