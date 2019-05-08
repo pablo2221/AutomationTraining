248f12eef9df5fd9fcd6379394f7008073478e15
Pablo Quiros <pablo.2221@hotmail.com>
2019-05-08 16:55:09 -0600

    add
    a

---
+++ src/main/java/utilities/Reader.java
@@ -0,0 +1,25 @@
+package utilities;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class Reader {
+
+    Properties props = new Properties();
+    public String readProperty (String key){
+
+
+        String result= "";
+        try{
+
+            props.load(new FileInputStream("config.properties"));
+            result= props.getProperty(key);
+
+        }catch (Exception e){
+
+            System.out.println("No se pudo leer la propiedad");
+
+        }
+        return result;
+    }
+}
