b1f03b3d717ac79d8658f470b6b90cd561740913
Pablo Quiros <pablo.2221@hotmail.com>
2019-05-08 15:46:25 -0600

    add scenarios
    added new scenarios

--- src/test/java/features/login.feature
+++ src/test/java/features/login.feature
@@ -13,7 +13,7 @@ Feature: Login Functionality
     And I click the sign in button
     Then I see the account page displayed
 
-    # el Escenario de abajo es del proyecto final de automation fundamentals
+    # a partir de aca son escenarios del curso de fundamentals
 
   Scenario: Verify error message
     Given I am on Login Page of "http://automationpractice.com/index.php"
@@ -21,4 +21,24 @@ Feature: Login Functionality
     And I press the create account button
     Then I validate the error message displayed "Invalid email address."
 
+    # El escenario corresponde a validar que al colocar datos de una cuenta no creada se muestre un msj de error y se compare con el esperado
 
+  Scenario: Verify Sign in error message
+    Given I am on Login Page of "http://automationpractice.com/index.php"
+    When I Press the Login button
+    And I enter the email address "pablo.2221@hotmail.com"
+    And I enter the password "passwordGeneric"
+    And I press the Sign in button "SubmitLogin"
+    Then I validate error message "Authentication failed."
+
+  # Esta prueba simula el proceso de enviar un msj a servicio al cliente agregando un file upload y valida el msj de confirmacion
+
+  Scenario: Verify process to send a message to customer service including a .png attachment
+    Given I navigate to contact us page "http://automationpractice.com/index.php?controller=contact"
+    When I select from the dropdown the index 1
+    And I enter the email address "pablo.2221@hotmail.com"
+    And I enter the order reference "id_order", "123456"
+    And I enter the message "message", "This is a test to validate"
+    And I upload the file "fileUpload", "/Users/it/Downloads/test.png"
+    And I press the send message button "submitMessage"
+    Then I validate error message "Your message has been successfully sent to our team.", "/html/body/div/div[2]/div/div[3]/div/p"
--- src/test/java/specDef/loginSpecDef.java
+++ src/test/java/specDef/loginSpecDef.java
@@ -6,6 +6,8 @@ import cucumber.api.java.en.And;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import utilities.driverProvider;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -103,6 +105,102 @@ public class loginSpecDef {
             org.testng.Assert.fail("no se encontro un elemento para validar");
 
         }
+
+    }
+
+
+    @And("I enter the email address {string}")
+    public void iEnterTheEmailAddress(String emailAddress) {
+
+        driver.findElement(By.id("email")).sendKeys(emailAddress);
+
+    }
+
+    @And("I enter the password {string}")
+    public void iEnterThePassword(String genericPassword) {
+
+        driver.findElement(By.id("passwd")).sendKeys(genericPassword);
+
+    }
+
+    @Then("I validate error message {string}")
+    public void iValidateErrorMessage(String errorMessage) {
+
+        try {
+
+            Thread.sleep(3000);
+            String errorMessage2 = driver.findElement(By.xpath("/html/body/div/div[2]/div/div[3]/div/div[1]/ol/li")).getText();
+            org.testng.Assert.assertEquals(errorMessage2,errorMessage);
+
+        }
+
+        catch (NoSuchElementException | InterruptedException ex){
+
+            org.testng.Assert.fail("no se encontro un elemento para validar");
+
+        }
+
+    }
+
+
+    @And("I press the Sign in button {string}")
+    public void iPressTheSignInButton(String buttonId) {
+
+        driver.findElement(By.id(buttonId)).click();
+
+    }
+
+    @Given("I navigate to contact us page {string}")
+    public void iNavigateToContactUsPage(String url) {
+
+        driver.get(url);
+
+    }
+
+    @When("I select from the dropdown the index {int}")
+    public void iSelectFromTheDropdownTheIndex(int index) throws InterruptedException {
+
+        Select dropDown = new Select(driver.findElement(By.xpath("//*[@id=\"id_contact\"]")));
+        dropDown.selectByIndex(1);
+        Thread.sleep(3000);
+
+    }
+
+    @And("I enter the order reference {string}, {string}")
+    public void iEnterTheOrderReference(String orderReferenceId, String orderNumber) {
+
+        driver.findElement(By.id(orderReferenceId)).sendKeys(orderNumber);
+
+    }
+
+    @And("I enter the message {string}, {string}")
+    public void iEnterTheMessage(String messageFieldId, String message) throws InterruptedException {
+
+        driver.findElement(By.id(messageFieldId)).sendKeys(message);
+        Thread.sleep(3000);
+
+    }
+
+    @And("I upload the file {string}, {string}")
+    public void iUploadTheFile(String uploadFileId, String fileExtension) {
+
+        WebElement uploadFile = driver.findElement(By.id(uploadFileId));
+        uploadFile.sendKeys(fileExtension);
+
+    }
+
+    @And("I press the send message button {string}")
+    public void iPressTheSendMessageButton(String sendMessageButtonID) {
+
+        driver.findElement(By.id(sendMessageButtonID)).click();
+
+    }
+
+    @Then("I validate error message {string}, {string}")
+    public void iValidateErrorMessage(String expectedMessage, String xpath) {
+
+        String message = driver.findElement(By.xpath(xpath)).getText();
+        org.testng.Assert.assertEquals(message, expectedMessage);
 
     }
 }
